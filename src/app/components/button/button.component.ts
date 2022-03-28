36c00484d5a65eedebe4675d477c3603cfa02b1d
Cian Twyford <cian.twyford@outlook.ie>
2022-03-28 18:13:31 +0100

    Task Components

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
 
+  @Input() text: string | undefined;
+  @Input() color: string | undefined;
+  @Output() btnClick = new EventEmitter()
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onClick()
+  {
+    this.btnClick.emit();
   }
 
 }
